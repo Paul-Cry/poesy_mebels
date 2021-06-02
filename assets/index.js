5fd33b1b511eaaea06b1b5d050054c094617164c
Yar-nikita <74370771+Yar-nikita@users.noreply.github.com>
2021-06-02 08:57:36 +0500

    ''

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,11 @@ let action = function(number){
 window.onscroll = () => {
 	console.log(window.pageYOffset)
 	app.number = window.pageYOffset;
+	if( window.pageYOffset >= 408)
+		app.hide = 'opacity: 1;'
 }
+
+
 
 
 
@@ -14,11 +18,24 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		number: 23,
+		hide: 'opacity: 0;',
+		check: true,
+		check_click: true
 	},
 	methods: {
 		changeWind(number){
-			action(number)
-			console.log(23)
+			action(number);
+			console.log(23);
+		},
+		change_chek(option){
+			if(option == 'left'){
+				this.check = false;
+				this.check_click = false;
+			}else{
+				this.check= true;
+				this.check_click = true;
+			}
+			
 		}
 	}
 })
